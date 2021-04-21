64a372bfb8338764f5586343c1bbfecaf0779f26
INSANEANDSAD <81450153+INSANEANDSAD@users.noreply.github.com>
2021-04-21 20:35:17 +1200

    Update flamethrower.js

--- scripts/blocks/turret/flamethrower.js
+++ scripts/blocks/turret/flamethrower.js
@@ -2,7 +2,8 @@
 const flamethrower = extendContent(ItemTurret, "flamethrower", {
     init(){
 		this.ammo(
-		    Items.pyratite, flamePyra 
+		    Items.pyratite, flamePyra,
+		    Vars.content.getByName(ContentType.item, "name-HE"), flameExplosive
 		);
 		this.super$init();
 	  }
@@ -11,7 +12,14 @@ const flamethrower = extendContent(ItemTurret, "flamethrower", {
 const pyraShoot = new Effect(34, e => {
 	Draw.color(Pal.lightPyraFlame, Pal.darkPyraFlame, Color.gray, e.fin());
     Angles.randLenVectors(e.id, 32, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
-        Fill.circle(e.x + x, e.y + y, 1.2 + e.fout() * 2.6);
+        Fill.circle(e.x + x, e.y + y, 1.2 + e.fout() * 2.4);
+    }});
+});
+
+const blastShoot = new Effect(34, e => {
+	Draw.color(Pal.missileYellow, Pal.missileYellowBack, Color.gray, e.fin());
+    Angles.randLenVectors(e.id, 34, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
+        Fill.circle(e.x + x, e.y + y, 1.55 + e.fout() * 2.5);
     }});
 });
 
@@ -24,10 +32,28 @@ flamePyra.width=6;
 flamePyra.speed=6;
 flamePyra.pierce=true;
 flamePyra.collidesAir=true;
+flamePyra.ammoMultiplier=9;
 flamePyra.statusDuration=360;
 flamePyra.shootEffect=pyraShoot;
 flamePyra.hitEffect=Fx.hitFlameSmall;
 flamePyra.despawnEffect=Fx.none;
 flamePyra.status=StatusEffects.burning;
 flamePyra.hittable=false;
+
+const flameExplosive=extend(BulletType,{});
+flameExplosive.damage=80;
+flameExplosive.hitSize=7;
+flameExplosive.lifetime=32;
+flameExplosive.length=12;
+flameExplosive.width=6;
+flameExplosive.speed=6;
+flameExplosive.pierce=true;
+flameExplosive.ammoMultiplier=12;
+flameExplosive.collidesAir=true;
+flameExplosive.statusDuration=360;
+flameExplosive.shootEffect=pyraShoot;
+flameExplosive.hitEffect=Fx.hitFlameSmall;
+flameExplosive.despawnEffect=Fx.none;
+flameExplosive.status=StatusEffects.burning;
+flameExplosive.hittable=false;
 
