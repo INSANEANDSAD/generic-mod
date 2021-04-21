76b5f39fa93b188812b33b35817c58da2d6e7eed
INSANEANDSAD <81450153+INSANEANDSAD@users.noreply.github.com>
2021-04-21 20:50:20 +1200

    BALANCED

--- scripts/blocks/turret/flamethrower.js
+++ scripts/blocks/turret/flamethrower.js
@@ -3,6 +3,7 @@ const flamethrower = extendContent(ItemTurret, "flamethrower", {
     init(){
 		this.ammo(
 		    Items.pyratite, flamePyra,
+		    Items.surgeAlloy, flameSurge,
 		    Vars.content.getByName(ContentType.item, "name-HE"), flameExplosive
 		);
 		this.super$init();
@@ -24,7 +25,7 @@ const blastShoot = new Effect(34, e => {
 });
 
 const flamePyra=extend(BulletType,{});
-flamePyra.damage=55;
+flamePyra.damage=57;
 flamePyra.hitSize=7;
 flamePyra.lifetime=32;
 flamePyra.length=12;
@@ -41,7 +42,7 @@ flamePyra.status=StatusEffects.burning;
 flamePyra.hittable=false;
 
 const flameExplosive=extend(BulletType,{});
-flameExplosive.damage=80;
+flameExplosive.damage=88;
 flameExplosive.hitSize=7;
 flameExplosive.lifetime=32;
 flameExplosive.length=12;
@@ -51,9 +52,31 @@ flameExplosive.pierce=true;
 flameExplosive.ammoMultiplier=12;
 flameExplosive.collidesAir=true;
 flameExplosive.statusDuration=360;
-flameExplosive.shootEffect=pyraShoot;
+flameExplosive.shootEffect=blastShoot;
 flameExplosive.hitEffect=Fx.hitFlameSmall;
 flameExplosive.despawnEffect=Fx.none;
 flameExplosive.status=StatusEffects.burning;
 flameExplosive.hittable=false;
+
+const flameSurge=extend(BulletType,{});
+flameSurge.damage=70;
+flameSurge.lightning=2;
+flameSurge.lightningDamage=24;
+flameSurge.lightningLength=8;
+flameSurge.hitSize=7;
+flameSurge.lifetime=32;
+flameSurge.length=12;
+flameSurge.width=6;
+flameSurge.speed=6;
+flameSurge.pierce=true;
+flameSurge.ammoMultiplier=12;
+flameSurge.collidesAir=true;
+flameSurge.statusDuration=360;
+flameSurge.shootEffect=pyraShoot;
+flameSurge.hitEffect=Fx.hitFlameSmall;
+flameSurge.despawnEffect=Fx.none;
+flameSurge.status=StatusEffects.burning;
+flameSurge.hittable=false;
+
+
 
