c314f33c7d11f2b7862d1e44b3b723da34bc071f
INSANEANDSAD <81450153+INSANEANDSAD@users.noreply.github.com>
2021-04-22 12:44:29 +1200

    Tweaks

--- scripts/blocks/turret/flamethrower.js
+++ scripts/blocks/turret/flamethrower.js
@@ -12,20 +12,20 @@ const flamethrower = extendContent(ItemTurret, "flamethrower", {
 
 const pyraShoot = new Effect(34, e => {
 	Draw.color(Pal.lightPyraFlame, Pal.darkPyraFlame, Color.gray, e.fin());
-    Angles.randLenVectors(e.id, 32, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
+    Angles.randLenVectors(e.id, 30, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
         Fill.circle(e.x + x, e.y + y, 1.2 + e.fout() * 2.4);
     }});
 });
 
-const blastShoot = new Effect(34, e => {
+const blastShoot = new Effect(37, e => {
 	Draw.color(Pal.missileYellow, Pal.missileYellowBack, Color.gray, e.fin());
-    Angles.randLenVectors(e.id, 34, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
+    Angles.randLenVectors(e.id, 33, e.finpow() * 190, e.rotation, 6, new Floatc2(){get: (x, y) => {
         Fill.circle(e.x + x, e.y + y, 1.55 + e.fout() * 2.5);
     }});
 });
 
 const flamePyra=extend(BulletType,{});
-flamePyra.damage=57;
+flamePyra.damage=55;
 flamePyra.hitSize=7;
 flamePyra.lifetime=32;
 flamePyra.length=12;
@@ -33,7 +33,7 @@ flamePyra.width=6;
 flamePyra.speed=6;
 flamePyra.pierce=true;
 flamePyra.collidesAir=true;
-flamePyra.ammoMultiplier=9;
+flamePyra.ammoMultiplier=5;
 flamePyra.statusDuration=360;
 flamePyra.shootEffect=pyraShoot;
 flamePyra.hitEffect=Fx.hitFlameSmall;
@@ -42,14 +42,14 @@ flamePyra.status=StatusEffects.burning;
 flamePyra.hittable=false;
 
 const flameExplosive=extend(BulletType,{});
-flameExplosive.damage=88;
+flameExplosive.damage=70;
 flameExplosive.hitSize=7;
 flameExplosive.lifetime=32;
 flameExplosive.length=12;
 flameExplosive.width=6;
 flameExplosive.speed=6;
 flameExplosive.pierce=true;
-flameExplosive.ammoMultiplier=12;
+flameExplosive.ammoMultiplier=7;
 flameExplosive.collidesAir=true;
 flameExplosive.statusDuration=360;
 flameExplosive.shootEffect=blastShoot;
@@ -59,7 +59,7 @@ flameExplosive.status=StatusEffects.burning;
 flameExplosive.hittable=false;
 
 const flameSurge=extend(BulletType,{});
-flameSurge.damage=70;
+flameSurge.damage=75;
 flameSurge.lightning=2;
 flameSurge.lightningDamage=24;
 flameSurge.lightningLength=8;
@@ -69,10 +69,10 @@ flameSurge.length=12;
 flameSurge.width=6;
 flameSurge.speed=6;
 flameSurge.pierce=true;
-flameSurge.ammoMultiplier=12;
+flameSurge.ammoMultiplier=6;
 flameSurge.collidesAir=true;
 flameSurge.statusDuration=360;
-flameSurge.shootEffect=pyraShoot;
+flameSurge.shootEffect=blastShoot;
 flameSurge.hitEffect=Fx.hitFlameSmall;
 flameSurge.despawnEffect=Fx.none;
 flameSurge.status=StatusEffects.burning;
