983eba4e9093647fe7bad0c9dc9ac52e872610f1
INSANEANDSAD <81450153+INSANEANDSAD@users.noreply.github.com>
2021-05-10 09:31:39 +1200

    balans

--- scripts/blocks/turret/black-hole.js
+++ scripts/blocks/turret/black-hole.js
@@ -16,7 +16,7 @@ const bhT=extend(BasicBulletType,{
   update(b){
         Units.nearbyEnemies(b.team, b.x - 132, b.y - 132, 132 * 2, 132 * 2, cons(unit => {
             if(unit.within(b.x, b.y, 132)){
-                unit.impulse(Tmp.v1.set(b).sub(unit).limit((132 + Interp.pow3In.apply(1 - (unit.dst(b) - 132) / 132) * 1.5)));
+                unit.impulse(Tmp.v1.set(b).sub(unit).limit((132 + Interp.pow3In.apply(1 - (unit.dst(b) - 132) / 132) * 2.5)));
                 if(b.timer.get(1, 6)){
                 Damage.damage(b.team,b.x,b.y,132,50*(Math.max(1 - ((unit.dst(b) -  132)/132), 0.2)));
                 }
@@ -48,14 +48,34 @@ const bhS=extend(BasicBulletType,{
   update(b){
         Units.nearbyEnemies(b.team, b.x - 280, b.y - 280, 280 * 2, 280 * 2, cons(unit => {
             if(unit.within(b.x, b.y, 280)){
-                unit.impulse(Tmp.v1.set(b).sub(unit).limit((280 + Interp.pow3In.apply(1 - (unit.dst(b) - 280) / 280) * 8.5)));
-                if(b.timer.get(0,6)){ 
-                Damage.damage(b.team,b.x,b.y,280,120*(Math.max(1 - ((unit.dst(b) -  280)/280), 0.05)));
-                }
+                unit.impulse(Tmp.v1.set(b).sub(unit).limit((280 + Interp.pow3In.apply(1 - (unit.dst(b) - 280) / 280) * 10)));
             };
         }));
+        if(b.timer.get(0,5)){ 
+            Units.nearbyEnemies(b.team, b.x - 280, b.y - 280, 280 * 2, 280 * 2, cons(unit => {
+                if(unit.within(b.x, b.y, 280)){
+                    Damage.damage(b.team,b.x,b.y,280,40*(Math.max(1 - ((unit.dst(b) - 280)/280), 0.05)));
+                    unit.health -= 20; 
+                };
+         })); 
+        Damage.tileDamage(b.team,World.toTile(b.x),World.toTile(b.y),224,50);
+         Units.nearbyEnemies(b.team, b.x - 64, b.y - 64, 64 * 2, 64 * 2, cons(unit => {
+                if(unit.within(b.x, b.y, 64)){
+                    unit.health -= unit.health * 0.005; 
+                };
+         })); 
     }
-});
+}});
+
+/* Vars.world.raycastEach(Vars.World.toTile(b.LastX), Vars.World.toTile(b.lastY), b.tileX(), b.tileY(), (x, y) => {
+                const tile = Vars.world.build(x, y);
+                if(tile == null) return;
+                if(tile.collide(b) && !tile.dead && tile.team != b.team){
+                    this.hitTile(b, tile, tile.health, true);
+                }
+                return false;
+            });
+*/
 
 /*
 bhS.suckR=280;
@@ -64,7 +84,9 @@ bhS.suckDscl=0.05;
 bhS.suckS=8.5;
 */
 bhS.damageInterval=5;
-bhS.damage=132;
+bhS.damage=120;
+bhS.splashDamage=40;
+bhS.splashDamageRadius=40;
 bhS.trailEffect=Fx.massiveExplosion;
 bhS.pierce=true;
 bhS.speed=2;
